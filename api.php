9ce77b385dd036f84314548247695a3c55259ec2
Emmo65 <emrah.sahin@web.de>
2026-03-30 11:23:21 +0200

    ERstellung des Backends für die Webanwendung

---
+++ api.php
@@ -0,0 +1,73 @@
+<?php
+header("Content-Type: application/json");
+require_once "db.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+if ($method == "GET") {
+
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+
+        $sql = "SELECT * FROM produkte WHERE id = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
+        $produkt = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($produkt);
+
+    } else {
+        $sql = "SELECT * FROM produkte";
+        $stmt = $pdo->query($sql);
+        $produkte = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($produkte);
+    }
+
+
+}
+if ($method == "POST") {
+    $datei = file_get_contents("php://input");
+    $json=json_decode($datei, true);
+    $name = $json["name"];
+    $preis = $json["preis"];
+    $beschreibung = $json["beschreibung"];
+
+    $sql = "INSERT INTO produkte (name, preis, beschreibung) VALUES (?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$name, $preis, $beschreibung]);
+
+    echo json_encode(["message" => "Produkt wurde erstellt"]);
+}
+if ($method == "PUT") {
+    $datei = file_get_contents("php://input");
+    $json = json_decode($datei, true);
+
+    $id = $json["id"];
+    $name = $json["name"];
+    $preis = $json["preis"];
+    $beschreibung = $json["beschreibung"];
+
+    $sql = "UPDATE produkte SET name = ?, preis = ?, beschreibung = ? WHERE id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$name, $preis, $beschreibung, $id]);
+
+    echo json_encode(["message" => "Produkt wurde aktualisiert"]);
+}
+if ($method == "DELETE") {
+     $datei = file_get_contents("php://input");
+    $json = json_decode($datei, true);
+
+    $id = $json["id"];
+    if (!isset($id)) {
+         echo json_encode(["message" => "Produkt nicht gefunden."]);
+    }
+    else{
+      
+        $sql = "DELETE FROM produkte WHERE id=?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
+        echo json_encode(["message"=>"Produkt wurde erfolgreich gelöscht."]);
+    }
+}
+?>
